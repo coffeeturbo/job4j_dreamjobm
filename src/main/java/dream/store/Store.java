331b4f94ec7fb6ad4c48e64e698450cea2b5db19
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2021-01-23 15:05:25 +0300

    CandidateSerlvet. Создание кандидата. [#438668]

--- src/main/java/dream/store/Store.java
+++ src/main/java/dream/store/Store.java
@@ -43,4 +43,9 @@ public class Store {
         posts.put(post.getId(), post);
     }
 
+    public void save(Candidate candidate) {
+        candidate.setId(POST_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
+    }
+
 }
