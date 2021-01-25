46a98655c1393cd05092c64658c3de7873948760
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2021-01-25 13:54:54 +0300

    MVC в Servlet. [#438678]

--- src/main/java/dream/servlet/IndexServlet.java
+++ src/main/java/dream/servlet/IndexServlet.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.sendRedirect(req.getContextPath() + "/index.jsp");
     }
 }
