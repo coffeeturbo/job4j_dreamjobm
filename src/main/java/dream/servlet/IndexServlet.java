c27a44c97e41cded102bbbe20293ca04aafc8d93
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2021-01-26 23:10:22 +0300

    MVC в Servlet. [#438678] review fix

--- src/main/java/dream/servlet/IndexServlet.java
+++ src/main/java/dream/servlet/IndexServlet.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect(req.getContextPath() + "/index.jsp");
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
